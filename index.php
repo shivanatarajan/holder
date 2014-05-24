2692b4f2b5ad2d3653b81e61642424bfbb5d3768
shivanatarajan <shivanatarajan87@gmail.com>
2014-05-25 04:06:43 +0800

    Basic Version

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- link rel="shortcut icon" href="../../assets/ico/favicon.ico" -->
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+
+
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Enjoy the Report</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Environment</a></li>
+            <li><a href="#">Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+  <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Entity Search...">
+          </form>
+            <li><a href="">Versioning</a></li>
+            <li><a href="">Data File</a></li>
+            <li><a href="">Data Joins</a></li>
+            <li><a href="">Data Logic Builder</a></li>
+            <li><a href="">Reports</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">User Management</a></li>
+            <li><a href="">System Health</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Tasks</h1>
+
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/90x70/sky" class="img-circle" >
+            <h4>Report Generator</h4>
+              <span class="text-muted">To Create a new Report</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/90x70/sky" class="img-circle" >
+            <h4>ROI Calculator</h4>
+              <span class="text-muted">Want to calculate your ROI ahead of time? Click here :)</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/90x70/sky" class="img-circle" >
+            <h4>Data Quality</h4>
+              <span class="text-muted">Checks the Quality of the Data</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/90x70/sky" class="img-circle" >
+
+            <h4>Execution time calculator</h4>
+              <span class="text-muted">Calculates the approx. total execution time for the report</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+            <img data-src="holder.js/90x70/sky" class="img-circle" >
+            <h4>Scheduler</h4>
+              <span class="text-muted">Schedules the report</span>
+            </div>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <script src="docs.min.js"></script>
+  </body>
+</html>
